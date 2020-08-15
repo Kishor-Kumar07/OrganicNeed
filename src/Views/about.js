601b206c507049ac69bfc9ab7fc2ed6122dea3fa
Kishor-Kumar07 <kishorck00@gmail.com>
2020-08-15 19:46:37 +0530

    commit

--- src/Views/about.js
+++ src/Views/about.js
@@ -2,7 +2,7 @@ import React from 'react'
  function About() {
     return (
         <div>
-            <div style={{paddingLeft:'70px',backgroundColor:'black'}}>
+            <div style={{paddingLeft:'70px',backgroundColor:'rgb(51, 163, 47)'}}>
                <h1 style={{color:'white'}}>Our Vision</h1>
             </div>
             <div style={{paddingLeft:'30px'}}>
@@ -11,27 +11,31 @@ import React from 'react'
 </h4>
 
             </div>
-            <div style={{paddingLeft:'70px',backgroundColor:'black'}}>
+            <div style={{paddingLeft:'70px',backgroundColor:'rgb(51, 163, 47)'}}>
                <h1 style={{color:'white'}}> Our Mission</h1>
             </div>
             <div style={{paddingLeft:'30px'}}>
 <h4>To be a trustworthy and innovative global leader in providing genuine organic True Wellness products and solutions for conscious, healthy living.
 </h4>
             </div>
-            <div style={{paddingLeft:'70px',backgroundColor:'black'}}>
+            <div style={{paddingLeft:'70px',backgroundColor:'rgb(51, 163, 47)'}}>
                <h1 style={{color:'white'}}> Our Values</h1>
             </div>
             <div style={{paddingLeft:'30px'}}>
-            <h4> Service to all</h4>
-            <h4>Total integrity</h4>
-            <h4>Absolute commitment to quality</h4>
-            <h4>Respect and devotion to Mother Nature</h4>
-            <h4>No compromise on being who we are</h4>
-            <h4>Our Commitment</h4>
-            <h4>To deliver genuine organic foods and products to our consumers.</h4>
-            <h4>To introduce a unique and successful business model that is committed to service and integrity, and benefits all.</h4>
-             <h4>To support natural, sustainable, organic, agriculture practices that serve and protect Mother Nature.</h4> 
-            <h4>To support the livelihood and well-being of farmers and tribal wildcrafters across rural India.</h4>
+            <h4>* Service to all</h4>
+            <h4>* Total integrity</h4>
+            <h4>* Absolute commitment to quality</h4>
+            <h4>* Respect and devotion to Mother Nature</h4>
+            <h4>* No compromise on being who we are</h4>
+            </div>
+            <div style={{paddingLeft:'70px',backgroundColor:'rgb(51, 163, 47)'}}>
+            <h4 style={{color:'white'}}>Our Commitment</h4>
+            </div>
+            <div style={{paddingLeft:'30px'}}>
+            <h4>* To deliver genuine organic foods and products to our consumers.</h4>
+            <h4>* To introduce a unique and successful business model that is committed to service and integrity, and benefits all.</h4>
+             <h4>* To support natural, sustainable, organic, agriculture practices that serve and protect Mother Nature.</h4> 
+            <h4>* To support the livelihood and well-being of farmers and tribal wildcrafters across rural India.</h4>
             </div>
 
         </div>
